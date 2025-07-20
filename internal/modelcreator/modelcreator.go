6cb684436e43f8e657b42a43e3194cb8153dcc4c
Ewhu <71063216+ewhu@users.noreply.github.com>
2025-07-20 19:27:43 +0700

    Add modelcreator.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ internal/modelcreator/modelcreator.go
@@ -0,0 +1,36 @@
+// internal/modelcreator/modelcreator.go
+package modelcreator
+
+import (
+	"log"
+	"os"
+)
+
+type App struct {
+	verbose bool
+	logger  *log.Logger
+}
+
+func NewApp(verbose bool) *App {
+	app := &App{
+		verbose: verbose,
+		logger:  log.New(os.Stdout, "", log.LstdFlags),
+	}
+
+	if verbose {
+		app.logger.SetPrefix("[DEBUG] ")
+	} else {
+		app.logger.SetPrefix("[INFO] ")
+	}
+
+	return app
+}
+
+func (a *App) Run() error {
+	a.logger.Printf("Starting ModelCreator processing")
+
+	// Add your implementation here
+
+	a.logger.Println("Processing completed successfully")
+	return nil
+}
